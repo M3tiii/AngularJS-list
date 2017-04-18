42fa5ebdee111a97be52906c89609d11be58bd26
M3tiii <m1995g@gmail.com>
2017-04-18 13:27:53 +0200

    Added edit/remove funcionality.

--- src/app/list/list.js
+++ src/app/list/list.js
@@ -1,7 +1,82 @@
 class listController {
 
-  constructor($scope) {
-    console.log(this, $scope);
+  constructor() {
+    this.isSelectedAll = false;
+    this.dataEdited = [];
+    // this.data = [];
+    // this.headers = [];
+    this.name = 'aaaaa';
+  }
+  sayHello() {
+    this.name = 'World';
+  }
+
+  onEditSelected() {
+    let anySelected = false;
+    this.data.forEach(item => {
+      if (item.isSelected) {
+        this.onEdit(item);
+        anySelected = true;
+      }
+    });
+    this.isSelectedAll = anySelected;
+  }
+
+  onSaveSelected() {
+    this.data.forEach(item => {
+      if (item.isSelected) {
+        this.onSave(item);
+      }
+    });
+    this.isSelectedAll = false;
+  }
+
+  onSelect(item) {
+    if (item.isEdited) {
+      this._backupItem(item);
+      this._unselectItem(item);
+    }
+  }
+
+  onEdit(item) {
+    this._selectItem(item);
+  }
+
+  onSave(item) {
+    this._unselectItem(item);
+  }
+
+  onDelete(item) {
+    this._unselectItem(item);
+    this._removeItemData(item, this.data);
+  }
+
+  _backupItem(item) {
+    const indexEdited = this.dataEdited.map(item => item.id).indexOf(item.id);
+    console.log(item, this.dataEdited[indexEdited]);
+    Object.assign(item, this.dataEdited[indexEdited]);
+  }
+
+  _removeItemData(item, data) {
+    const index = data.map(item => item.id).indexOf(item.id);
+    data.splice(index, 1);
+  }
+
+  _selectItem(item) {
+    item.isEdited = true;
+    item.isSelected = true;
+    this.dataEdited.push(Object.assign({}, item));
+  }
+
+  _unselectItem(item) {
+    item.isEdited = false;
+    item.isSelected = false;
+    this.isSelectedAll = this.isSelectedAll ? this._detectEdited() : false;
+    this._removeItemData(item, this.dataEdited);
+  }
+
+  _detectEdited() {
+    return this.data.filter(item => item.isEdited).length !== 0;
   }
 
 }
--- src/app/userList/userList.js
+++ src/app/userList/userList.js
@@ -3,27 +3,36 @@ class userListController {
   constructor($scope, $http) {
     this.headers = [{
       name: 'id',
-      title: 'id'
+      title: 'id',
+      type: 'text'
     }, {
       name: 'name',
-      title: 'name'
+      title: 'name',
+      type: 'text'
     }, {
       name: 'surname',
-      title: 'surname'
+      title: 'surname',
+      type: 'text'
     }, {
       name: 'date',
-      title: 'date of birth'
+      title: 'date of birth',
+      type: 'date'
     }, {
       name: 'number',
-      title: 'mobile number'
+      title: 'mobile number',
+      type: 'tel'
     }, {
       name: 'address',
-      title: 'address (city + street + house number)'
+      title: 'address (city + street + house number)',
+      type: 'text'
     }];
 
     $http
       .get('app/userList/users.json')
       .then(response => {
+        response.data.forEach(item => {
+          item.date = new Date(item.date);
+        });
         this.users = response.data;
       });
   }
