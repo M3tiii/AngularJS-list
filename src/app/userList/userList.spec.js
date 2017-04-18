687a1fabfac2c45adb189200f53d2bcfe664b2ae
M3tiii <m1995g@gmail.com>
2017-04-18 13:26:28 +0200

    Added test: User list should render list.

---
+++ src/app/userList/userList.spec.js
@@ -0,0 +1,41 @@
+import angular from 'angular';
+import 'angular-mocks';
+import {
+  userList
+} from './userList';
+
+const usersJson = [{
+  id: '0',
+  name: 'name0'
+}, {
+  id: '1',
+  name: 'name1'
+}];
+
+describe('user-list component', () => {
+  beforeEach(() => {
+    angular
+      .module('userList', ['app/userList/userList.html'])
+      .component('userList', userList);
+    angular.mock.module('userList');
+  });
+
+  it('list should get users and render <list>', angular.mock.inject(($rootScope, $compile, $httpBackend) => {
+    $httpBackend.when('GET', 'app/userList/users.json').respond(usersJson);
+    // $rootScope.headers = [{
+    //   name: 'id',
+    //   title: 'id',
+    //   type: 'text'
+    // }, {
+    //   name: 'name',
+    //   title: 'name',
+    //   type: 'text'
+    // }];
+
+    const element = $compile('<user-list></user-list>')($rootScope);
+    $httpBackend.flush();
+    $rootScope.$digest();
+    const users = element.find('list');
+    expect(users.length).toEqual(1);
+  }));
+});
