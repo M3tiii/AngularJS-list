d73069718dc9cc348af169bbf067519acd2a467d
M3tiii <m1995g@gmail.com>
2017-04-18 13:25:35 +0200

    Added test for list.

---
+++ src/app/list/list.spec.js
@@ -0,0 +1,51 @@
+import angular from 'angular';
+import 'angular-mocks';
+import {
+  list
+} from './list';
+
+describe('list component', () => {
+  beforeEach(() => {
+    angular
+      .module('list', ['app/list/list.html'])
+      .component('list', list);
+    angular.mock.module('list');
+  });
+
+  it('should render table', angular.mock.inject(($rootScope, $compile) => {
+    const scope = $rootScope;
+    const element = $compile('<list></list>')(scope);
+    scope.$apply();
+    scope.$digest();
+    expect(element.find('table').length).toEqual(1);
+  }));
+
+  it('should render 2 elements <list>', angular.mock.inject(($rootScope, $compile) => {
+    const scope = $rootScope.$new();
+    const element = $compile('<list headers="headers" data="data"></list>')(scope);
+
+    scope.data = [{
+      id: '0',
+      name: 'name0'
+    }, {
+      id: '1',
+      name: 'name1'
+    }];
+
+    scope.headers = [{
+      name: 'id',
+      title: 'id',
+      type: 'text'
+    }, {
+      name: 'name',
+      title: 'name',
+      type: 'text'
+    }];
+
+    scope.$apply();
+    scope.$digest();
+
+    const users = element[0].querySelectorAll('.item-row');
+    expect(users.length).toEqual(2);
+  }));
+});
